ede7ccb1e02765ea6b64e0c2dff52d9caa9c948c
kaanyaltiunity <kaan.yalti@unity3d.com>
2022-09-09 21:42:52 +0300

    travis yaml and deploy sh

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t kaanyalti/multi-client:latest -t kaanyalti/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kaanyalti/multi-server:latest -t kaanyalti/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kaanyalti/multi-worker:latest -t kaanyalti/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push kaanyalti/multi-client:latest
+docker push kaanyalti/multi-client:$SHA
+docker push kaanyalti/multi-server:latest
+docker push kaanyalti/multi-server:$SHA
+docker push kaanyalti/multi-worker:latest
+docker push kaanyalti/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=cygnetops/multi-server-pgfix-5-11
+kubectl set image deployments/client-deployment client=kaanyalti/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=kaanyalti/multi-worker:$SHA
